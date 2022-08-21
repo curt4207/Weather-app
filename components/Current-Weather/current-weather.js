aab00d3fce7fe7c990bc8d2eaa53c910a17bf57e
Curtis <Curt122181@gmail.com>
2022-08-21 10:12:56 -0400

    Removed unused code

--- components/Current-Weather/current-weather.js
+++ components/Current-Weather/current-weather.js
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
 import style from "../../styles/Home.module.css";
 
 const CurrentWeather = ({weatherData}) => {
-const [currentWeather, setCurrentWeather] = useState("");
-
 
 const properties = weatherData.properties;
 const periods = properties.periods;
 const firstPeriod = periods[0];
-// i want it to show the current weather for my location, the temp, if its sunny or partly cloudy, and maybe the high and low for the day
+
 return (
     <ul className={style.grid}>
         <li className={style.card} key={periods.startTime}>
