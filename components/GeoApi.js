90a31ce15ec532092b5e87a758d26ce55b08bc3e
12ahnaf34 <12ahnaf34@gmail.com>
2022-08-24 13:31:43 -0400

    Made requested changes, removed extra function

--- components/GeoApi.js
+++ components/GeoApi.js
@@ -1,45 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { resolve } from "styled-jsx/css";
 import style from "../styles/Home.module.css";
 
 function GeoApi(props) {
   const { weatherForecast, setWeatherForecast } = props;
   const [locationName, setLocationName] = useState("Linn, Kansas");
   const [searchTerm, setSearchTerm] = useState("");
+  const API_KEY = process.env.NEXT_PUBLIC_GEOLOCATION_API_KEY;
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
+  //Just keeping this function to help with development, will delete later
   async function fetchCoords() {
-    let long = 0;
-    let lat = 0;
-    let firstRes = await fetch(`http://api.positionstack.com/v1/forward?access_key=${process.env.NEXT_PUBLIC_GEOLOCATION_API_KEY}&query=${searchTerm}`);
+    if (API_KEY === null) {
+      console.log("Geolocation API key not set.");
+    }
+    let firstRes = await fetch(`http://api.positionstack.com/v1/forward?access_key=${API_KEY}&query=${searchTerm}`);
     let firstData = await firstRes.json();
     let a = 0;
     while (firstData.data[a].continent !== "North America") {
       a++;
     }
-    long = firstData.data[a].longitude;
-    lat = firstData.data[a].latitude;
+    const long = firstData.data[a].longitude;
+    const lat = firstData.data[a].latitude;
     console.log(a, firstData);
+    return { long, lat };
   }
 
-  async function fetchWeatherForecast() {
-    const res = await fetch(`https://api.weather.gov/gridpoints/${grid.office}/${parseInt(grid.gridX, 10)},${parseInt(grid.gridY, 10)}/forecast?units=us`);
-    const data = res.json().then((res) => {
-      console.log(res);
-      setWeatherData(res);
-    });
-  }
-
+  //Gets weather data
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    //Step 1 -  Fetches the longitude and latitude
     let long = 0;
     let lat = 0;
     let a = 0;
 
-    const firstRes = await fetch(`http://api.positionstack.com/v1/forward?access_key=${process.env.NEXT_PUBLIC_GEOLOCATION_API_KEY}&query=${searchTerm}`);
+    const firstRes = await fetch(`http://api.positionstack.com/v1/forward?access_key=${API_KEY}&query=${searchTerm}`);
     const firstData = await firstRes.json().then((firstData) => {
       while (firstData.data[a].continent !== "North America") {
         a++;
@@ -49,13 +47,16 @@ function GeoApi(props) {
       setLocationName(firstData.data[a].label);
     });
 
+    //Step 2 - Fetches grid data
     const secondRes = await fetch(`https://api.weather.gov/points/${lat},${long}`);
     const secondData = await secondRes.json();
-    // console.log(data);
+
     const office = secondData.properties.gridId;
     const gridX = secondData.properties.gridX;
     const gridY = secondData.properties.gridY;
+    console.log(office, gridX, gridY);
 
+    //Step 3 - Fetches weather forecast data
     const finalRes = await fetch(`https://api.weather.gov/gridpoints/${office}/${gridX},${gridY}/forecast?units=us`);
     const data = finalRes.json().then((res) => {
       setWeatherForecast(res);
